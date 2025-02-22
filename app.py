88c8ee0e100e414696a0dce774bded4e141fb4e1
phrp22 <pedro.clave22@gmail.com>
2025-02-22 18:37:12 +0000

    pequeno erro

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def main():
     st.sidebar.button("Sair", on_click=logout)  # ✅ Mantemos sem st.rerun()
 
     if st.session_state.user_type == "Profissional":
-    opcao = st.sidebar.radio("Menu", ["Dashboard", "Enviar Escala Psicométrica"])
-
-    if opcao == "Dashboard":
-        profissional.profissional_dashboard()
-    elif opcao == "Enviar Escala Psicométrica":
-        profissional.enviar_escala_interface()
+        opcao = st.sidebar.radio("Menu", ["Dashboard", "Enviar Escala Psicométrica"])
+        
+        if opcao == "Dashboard":
+            profissional.profissional_dashboard()
+        elif opcao == "Enviar Escala Psicométrica":
+            profissional.enviar_escala_interface()
 
 def login():
     st.subheader("Tela de Login")
