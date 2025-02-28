fe6db57ce2049ebe6b2e0eaeb6cc0aca2dc81d46
phrp22 <pedro.clave22@gmail.com>
2025-02-28 12:45:12 +0000

    vejamos

--- styles.py
+++ styles.py
@@ -1,4 +1,5 @@
-# styles.py
+# styles.py - Centraliza os estilos CSS
+
 BUTTON_STYLE = """
 <style>
     div.stButton > button:first-child {
@@ -15,28 +16,46 @@ BUTTON_STYLE = """
         text-align: center;
         box-shadow: 0px 0px 10px rgba(113, 89, 193, 0.5);
     }
+
+    /* Efeito Hover (passar o mouse) */
     div.stButton > button:first-child:hover {
         background-color: #5e47b0;
         transform: scale(1.05);
         box-shadow: 0px 0px 15px rgba(130, 94, 255, 0.7);
     }
+
+    /* Efeito Active (quando pressionado) */
     div.stButton > button:first-child:active {
-        background-color: #d32f2f !important;
-        border-color: #a82828 !important;
+        background-color: #d32f2f !important;  /* Vermelho uniforme */
+        border-color: #a82828 !important;  /* Vermelho escuro para contraste */
         box-shadow: 0px 0px 10px rgba(211, 47, 47, 0.8);
         transform: scale(0.98);
     }
 </style>
 """
 
+
 TITLE_STYLE = """
 <style>
     h1 {
-        font-size: 42px;
+        font-size: 38px;
         font-weight: bold;
-        text-align: left; 
-        color: #ffffff !important; /* Título em branco */
+        text-align: left;  /* Alinhado à esquerda */
+        color: #5a69c9;  /* Tom roxo mais azulado e profissional */
+        margin-bottom: 10px;  /* Pequeno espaçamento inferior */
     }
+</style>
+"""
+
+GLOBAL_FONT_STYLE = """
+<style>
+/* Importe a fonte do Google Fonts */
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+/* Aplica a fonte em todos os elementos da página */
+html, body, [class*="css"]  {
+    font-family: 'Open Sans', sans-serif;
+}
 </style>
 """
 
@@ -59,3 +78,5 @@ PAGE_BG_STYLE = """
     }
 </style>
 """
+
+
