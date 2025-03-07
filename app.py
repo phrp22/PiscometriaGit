cdcd1a50bdb285c922b219af6c509dda0bd9d857
phrp22 <pedro.clave22@gmail.com>
2025-03-07 23:17:20 +0000

    user has profile removed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from auth import get_user
 from main_layout import render_main_layout
 from dashboard import render_dashboard, render_professional_dashboard
-from profile import render_onboarding_questionnaire, user_has_profile
+from profile import render_onboarding_questionnaire
 from utils.design_utils import load_css
 from utils.professional_utils import is_professional_enabled
 from utils.user_utils import get_user_info
